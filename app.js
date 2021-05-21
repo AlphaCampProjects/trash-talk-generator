71e160462a35e139f67f2b6a1f37e647667a4d12
tinahung126 <tina126hung@gmail.com>
2021-05-21 20:19:35 +0800

    fix JS standard

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const generateSentence = require('./generate_ sentence');
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const generateSentence = require('./generate_ sentence')
 
 app.engine(
   'hbs',
@@ -12,25 +12,25 @@ app.engine(
     extname: '.hbs',
     helpers: {
       eq: function (v1, v2) {
-        return v1 === v2;
-      },
-    },
+        return v1 === v2
+      }
+    }
   })
-);
-app.set('view engine', 'hbs');
-app.use(express.static('public'));
+)
+app.set('view engine', 'hbs')
+app.use(express.static('public'))
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.render('index');
-});
+  res.render('index')
+})
 
 app.post('/', (req, res) => {
-  const { character } = req.body;
-  const sentence = generateSentence(character);
-  res.render('index', { sentence, character });
-});
+  const { character } = req.body
+  const sentence = generateSentence(character)
+  res.render('index', { sentence, character })
+})
 app.listen(port, () => {
-  console.log(`The express server is running on http://localhost:${port}`);
-});
+  console.log(`The express server is running on http://localhost:${port}`)
+})
