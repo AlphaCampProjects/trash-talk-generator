6442eb188398fd90400081132c720b3e3da2e131
tinahung126 <tina126hung@gmail.com>
2021-05-21 10:51:59 +0800

    feat: add body-parser

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('index');
