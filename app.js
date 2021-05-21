c7195920b82699ca3e302d561ddf01923c22bb48
tinahung126 <tina126hung@gmail.com>
2021-05-21 20:18:35 +0800

    feat: add generate sentence function

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const app = express();
 const port = 3000;
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const generateSentence = require('./generate_ sentence');
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine(
+  'hbs',
+  exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    helpers: {
+      eq: function (v1, v2) {
+        return v1 === v2;
+      },
+    },
+  })
+);
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
 
@@ -14,6 +26,11 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.post('/', (req, res) => {
+  const { character } = req.body;
+  const sentence = generateSentence(character);
+  res.render('index', { sentence, character });
+});
 app.listen(port, () => {
   console.log(`The express server is running on http://localhost:${port}`);
 });
